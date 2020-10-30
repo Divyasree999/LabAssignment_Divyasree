f6a85f64a422e8c9a9895e3cb8a5b57cafc3b0c6
Divyasree999 <73293637+Divyasree999@users.noreply.github.com>
2020-10-30 13:32:02 +0530

    Update RemoveDuplicate.java

--- LabAssignment2/src/lab2/RemoveDuplicate.java
+++ LabAssignment2/src/lab2/RemoveDuplicate.java
@@ -1,6 +1,5 @@
 package lab2;
 import java.util.Scanner;
-import java.util.Arrays;
 /**
 * <h1>Removal of Duplicate from integers</h1>
 * The RemoveDuplicate program implements an application that
@@ -14,27 +13,40 @@ public class RemoveDuplicate {
 
 	public static void main(String[] args)
 	{
-		Scanner s = new Scanner(System.in);
-		//Declaration and Initialization of arrays
-	    int[] a = new int[5];
-	    int[] b = new int[5];
-	    System.out.println("Enter the elements of the array ::");// Prints the elements that have given
-	    for(int i=0; i<5; i++) 
-	      {
-	         a[i] = s.nextInt();
-      	  }
-	    System.out.println("The array created is ::"+Arrays.toString(a)); //Prints the array created
-	      for(int i=0; i<5; i++) 
-	      {
-	         for (int j=0; j<5; j++) 
-	         {
-	        	 if(a[i]!=a[j])
-	             {
-	        		 b[i++]=a[i];
-	             }
-	         }
-	      }
-	      //Prints the elements after removing the elements
-	     System.out.println("Array after removing elements ::"+Arrays.toString(b));
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the size of an array");
+		int n=sc.nextInt();
+		System.out.println("Enter the element of an array");
+		int arr[]=new int[n];
+		for(int i=0;i<n;i++) {
+			arr[i]=sc.nextInt();
+		}
+		sc.close();
+		n=modifyArray(arr);
+		System.out.println("After Duplicate Element");
+		for (int i=0; i<n; i++) 
+			System.out.print(arr[i]+" "); 
+	}
+
+	private static int modifyArray(int[] arr) {
+		
+		int n=arr.length; //size of an array
+		if (n==0 || n==1){  //array is empty or contain 1 element
+            return n;  
+        }  
+        int[] temp = new int[n]; 
+        Arrays.sort(arr); //sorting array
+        int j = 0;  
+        for (int i=0; i<n-1; i++){  
+            if (arr[i] != arr[i+1]){  
+                temp[j++] = arr[i];  
+            }  
+         }  
+        temp[j++] = arr[n-1];     
+        // Changing original array  
+        for (int i=0; i<j; i++){  
+            arr[i] = temp[i];  
+        }  
+        return j; 
 	 }
 }
